e5f3c0ec14beeb88e09ab4ae74dca84788db4478
Domui <jgaudenzi711122@muhsdstudents.org>
2025-03-27 08:46:44 -0700

    Update Space invaders_Bonetti.py

--- Bonetti Pygame/Space Invaders/Space invaders_Bonetti.py
+++ Bonetti Pygame/Space Invaders/Space invaders_Bonetti.py
@@ -26,20 +26,46 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect.center = [x, y]
         self.health_start = health
         self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         speed = 8
+        cooldown = 650 #milliseconds
+        
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > 0:
             self.rect.x -= speed
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
             self.rect.x += speed
 
+        #recird current time
+        time_now = pygame.time.get_ticks()
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
+
         pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
         if self.health_remaining > 0:
             pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 15))
 
+
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+
+
 spaceship_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
 
 spaceship = Spaceship(int(screen_width / 2), screen_height - 100, 3)
 spaceship_group.add(spaceship)
@@ -57,7 +83,11 @@ while run:
             run = False
 
     spaceship.update()
+
+    bullet_group.update()
+    
     spaceship_group.draw(screen)
+    bullet_group.draw(screen)
 
     pygame.display.update()
     
